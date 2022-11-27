c9a0b33bf5a4a46cccec3569ad6af2dd0ab13c01
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-11-27 13:24:13 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,81 @@
+<?php 
+
+session_start(); 
+
+include "dbconnection.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['uname']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM credentiale WHERE nume='$uname' AND parola='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['nume'] === $uname && $row['parola'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['nume'] = $row['nume'];
+
+                header("Location: home.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
