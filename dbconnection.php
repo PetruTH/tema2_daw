12b759c525a27bc9829eaea4bd29bfbb17e73ae9
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-11-27 14:51:20 +0200

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -2,13 +2,13 @@
 
 $sname= "eu-cdbr-west-03.cleardb.net";
 
-$unmae= "b5a6be30d39473";
+$uname= "b5a6be30d39473";
 
 $password = "fa37249f";
 
 $db_name = "heroku_774a72d2049e8b8";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
 
