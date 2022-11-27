e567be5d455981dbe3a48a90a06af8374df2d249
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-11-27 14:47:26 +0200

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,14 +1,17 @@
 <?php
 
-$servername = "eu-cdbr-west-03.cleardb.net";
-$rootname = "b5a6be30d39473";
-$rootpassword = "fa37249f";
+$sname= "eu-cdbr-west-03.cleardb.net";
+
+$unmae= "b5a6be30d39473";
+
+$password = "fa37249f";
 
 $db_name = "heroku_774a72d2049e8b8";
-$conn = mysqli_connect($servername, $rootname, $rootpassword, $db_name);
 
-if(!$conn){
-    echo "Connection Failed";
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+
 }
-
-?>
