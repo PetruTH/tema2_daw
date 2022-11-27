5b77780532f9b441c7c074f1f73a00b0e94b35c2
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-11-27 15:10:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
         <form action="login.php" method="post">
 
-        <h2>LOGIN</h2>
+        <h2>LOGIN!!</h2>
 
         <?php if (isset($_GET['error'])) { ?>
             <p class="error"><?php echo $_GET['error']; ?></p>
